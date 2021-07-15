3a61891fc184862f15efe1933b539cde45f3e74d
Kartik Agarwal <kartik.agarwal.192.168@gmail.com>
2021-07-15 21:01:27 +0530

    Created Views And Public Directory with placeholders

--- MERNFullStack/index.js
+++ MERNFullStack/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 app.use(express.urlencoded({encoded: true}))
 const ejsMate = require('ejs-mate');
+app.use( express.static( "public" ) );
+app.use( express.static("views/CSS") );
 app.engine('ejs',ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'))
 app.get('/',(req, res)=>{
-    res.send("hi")
+    res.render('index')
 })
-app.listen(3000, ()=>{
+app.listen(3000, () =>{
     console.log("Server Started!")
 })
