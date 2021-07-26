680b348f2ff5675ed6932b8b485a63282da6688a
Kartik Agarwal <kartik.agarwal.192.168@gmail.com>
2021-07-26 18:42:28 +0530

    Login Route and Page Added

--- MERNFullStack/index.js
+++ MERNFullStack/index.js
@@ -11,6 +11,15 @@ app.set('views', path.join(__dirname,'views'))
 app.get('/',(req, res)=>{
     res.render('index')
 })
+app.get('/login',(req, res)=>{
+    res.render('login')
+})
+app.get('/signup',(req, res)=>{
+    res.render('signup')
+})
+app.get('/home',(req, res)=>{
+    res.render('home')
+})
 app.listen(3000, () =>{
     console.log("Server Started!")
 })
